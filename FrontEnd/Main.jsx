7a6afec326e1635517511f8b40bb9342a54b0aab
adititapariya <tapariyaaditi23@gmail.com>
2024-05-02 12:10:47 +0530

    components added

---
+++ FrontEnd/Main.jsx
@@ -0,0 +1,35 @@
+// rnfe -- create react native component
+// rafce -- create react component
+
+
+//safeareaview works in ios ..padding in android
+import { View, Text, SafeAreaView, StatusBar,Platform} from 'react-native'
+import React from 'react'
+import {NavigationContainer} from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './screens/Home';
+
+// const Main = () => {
+//   return (
+//     <View style={{paddingVertical: Platform.OS==="android"?40:0}}>
+//       <SafeAreaView>
+//         <Text>Main</Text>
+//         </SafeAreaView>  
+//     </View>
+//   )
+// }
+
+const Stack=createNativeStackNavigator()
+
+const Main = () => {
+  return <NavigationContainer>
+    <Stack.Navigator>
+        <Stack.Group>
+          <Stack.Screen name="Home" component={Home}/>  
+        </Stack.Group>
+    </Stack.Navigator>
+  </NavigationContainer>
+}
+
+
+export default Main;
---
+++ FrontEnd/screens/Home.jsx
@@ -0,0 +1,14 @@
+//rnfe
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home;
